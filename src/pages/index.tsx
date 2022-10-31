58fc86ae42792a57b29d4d4d7780fc184e353f98
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-10-31 11:14:06 +0100

    Load adventure data in multiple languages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,59 @@
 import { GetStaticProps } from "next";
+const yaml = require("js-yaml");
+import { type AdventureType } from "../types";
+
+const LANGUAGES = ["en", "nl"];
 
 type Props = {
-  greeting: string;
+  languages: {
+    en?: {
+      [key: string]: AdventureType;
+    };
+    nl?: {
+      [key: string]: AdventureType;
+    };
+  };
 };
 
 const App: React.FC<Props> = (props) => {
   return (
     <div>
-      <h1 className="text-3xl">{props.greeting}</h1>
+      {Object.keys(props.languages).map((lang) => {
+        return Object.keys(props.languages[lang]).map((adventure) => {
+          return Object.keys(props.languages[lang][adventure]).map((levels) => {
+            return (
+              <p>
+                {lang} {adventure} {levels}
+              </p>
+            );
+          });
+        });
+      })}
     </div>
   );
 };
 
 export default App;
 
-export const getStaticProps: GetStaticProps<Props> = () => {
-  return {
-    props: {
-      greeting: "Hello World!",
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  let data: Props = {
+    languages: {
+      en: undefined,
+      nl: undefined,
     },
+  };
+
+  for (let i = 0; i < LANGUAGES.length; i++) {
+    const lang = LANGUAGES[i];
+    const ldata = await fetch(
+      `https://raw.githubusercontent.com/hedyorg/hedy/main/content/adventures/${lang}.yaml`
+    ).then((response) => response.text());
+
+    const { adventures } = await yaml.load(ldata);
+    data.languages[lang] = adventures;
+  }
+
+  return {
+    props: data,
   };
 };
---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export interface AdventureType {
+  name: string;
+  description: string;
+  default_save_name: string;
+  levels: {
+    [key: string]: LevelType;
+  };
+}
+
+export interface LevelType {
+  intro_text?: "string";
+  story_text?: "string";
+  example_code?: "string";
+  story_text_2?: "string";
+  example_code_2?: "string";
+  start_code: "string";
+  story_text_3?: "string";
+  example_code_3?: "string";
+  story_text_4?: "string";
+  example_code_4?: "string";
+  story_text_5?: "string";
+  example_code_5?: "string";
+  story_text_6?: "string";
+  example_code_6?: "string";
+}
