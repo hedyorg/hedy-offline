68ae13938f7079c6f86289a9143299e019f2121d
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-11-15 14:37:42 +0100

    Make execute disabled when code has note changed

--- renderer/src/components/code-panel/code-panel.tsx
+++ renderer/src/components/code-panel/code-panel.tsx
@@ -1,10 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import AppContext from "../../app-context";
 import { FiZap } from "react-icons/fi";
 import dynamic from "next/dynamic";
-import { fetchHedy } from "../../helpers/fetchHedy";
-import getSkulpt from "../../helpers/getSkulpt";
-import { runPython } from "../../helpers/runPython";
 const CodeEditor = dynamic(() => import("../code-editor/code-editor"), { ssr: false });
 
 interface CodePanelType {}
@@ -12,18 +9,20 @@ interface CodePanelType {}
 const CodePanel: React.FC<CodePanelType> = (props) => {
   const appContext = useContext(AppContext);
   const [code, setCode] = useState(appContext.level.start_code);
+  const prevCode = useRef<string>("");
 
   return (
     <div className="w-full h-full relative">
       <CodeEditor setCode={(code) => setCode(code)} code={code} />
-
       <button
         onClick={async () => {
+          prevCode.current = code;
           appContext.setHedy(code);
         }}
-        className="px-6 group flex items-center gap-2 text-xl tracking-wide font-semibold py-2 absolute bottom-8 right-12 bg-white border border-neutral-300 rounded-lg"
+        disabled={code === prevCode.current}
+        className={` px-6 group flex items-center gap-2 text-xl tracking-wide font-semibold py-2 absolute bottom-8 right-12 bg-white border disabled:border-neutral-100 disabled:text-neutral-100 border-neutral-300 rounded-lg`}
       >
-        <FiZap className="group-hover:fill-[#E6D706] group-hover:text-[#E6D706] transition-all" size={"24px"} />
+        <FiZap className={`${code === prevCode.current ? "" : "group-hover:fill-[#E6D706] group-hover:text-[#E6D706]"} transition-all`} size={"24px"} />
         <span>Execute</span>
       </button>
     </div>
